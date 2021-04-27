66862e41a1f2e7c9e6a39003e2881f6533da62a1
alanwildan <76613101+alanwildan@users.noreply.github.com>
2021-04-27 09:31:02 +0700

    Baca

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install ffmpeg
+apt-get install tesseract
+npm install
+
+echo "DIA ATAS ERROR? JIKA ERROR SILAHKAN KETIK npm install JIKA TIDAK ERROR KANG KETIK npm start"
